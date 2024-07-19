66eebbc60397e93ffd8b5ac4fd01d3f5951734f0
nateBehailu <reanathanbehailu@gmail.com>
2024-07-19 16:15:49 +0300

    delete products from checkput when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,8 @@ cart.forEach((item) => {
         }
     });
 
-    cartSummaryHTML += `<div class="cart-item-container">
+    cartSummaryHTML += `
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id} ">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -95,5 +96,9 @@ document.querySelectorAll('.js-delete-quantity-link').forEach((link) => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     });
+    
 });
+
