b577a01b274316b85a6a8014972bea24f438b430
nateBehailu <reanathanbehailu@gmail.com>
2024-07-19 14:16:34 +0300

    use products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-    {
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        ratings : {
-            stars : 4.5,
-            count : 87
-        },
-        priceCent : 1090
-    },
-    {
-        image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        ratings : {
-            stars: 4,
-            ratings: 127
-        },
-        priceCent: 2095 
-    },
-    {
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        ratings: {
-            stars: 4.5,
-            ratings: 56
-        },
-        priceCent: 799
-    }
-];
-
 let productsHTML = '';
 products.forEach((product)=>{
     productsHTML +=
@@ -43,14 +13,14 @@ products.forEach((product)=>{
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.ratings.stars * 10}.png">
+              src="images/ratings/rating-${(product.rating.stars) * 10}.png">
             <div class="product-rating-count link-primary">
-                ${product.ratings.count}
+                ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceCent / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
