83dedf6beead48f3539b05f2a23e76ee308ff92e
nateBehailu <reanathanbehailu@gmail.com>
2024-07-20 14:55:30 +0300

    make delievery options interactive

--- data/cart.js
+++ data/cart.js
@@ -1,19 +1,27 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
     {
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 2,
+        deliveryOptionId: '1'
     },
     {
         productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
         quantity: 1,
+        deliveryOptionId: '2'
+
     }
 ];
+
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
  
 export function addToCart(productId){
     let matchingItem = null;
     cart.forEach((item)=>{
         if(productId === item.productId){
             matchingItem = item;
+            deliveryOptionId: '1'
         }
     });
 
@@ -26,6 +34,7 @@ export function addToCart(productId){
             quantity: 1
         });
     }
+    saveToStorage();
 }
 
 
@@ -37,4 +46,19 @@ export function removeFromCart(productId){
         }
     });
     cart = newCart;
+    saveToStorage();
+}
+
+
+export function updateDeliveryOption (productId, deliveryOptionId) {
+    let matchingItem = null;
+    cart.forEach((item)=>{
+        if(productId === item.productId){
+            matchingItem = item;
+            deliveryOptionId: '1'
+        }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    saveToStorage();
 }
